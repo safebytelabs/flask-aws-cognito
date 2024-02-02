f98cd9a1c31994cad9602c275d0272c04c40c684
Jonathan González <jgonf@safebytelabs.com>
2024-02-02 11:28:32 +0100

    .env implementation
    Added support for .env files

--- src/cognito/config.py
+++ src/cognito/config.py
@@ -1,13 +1,18 @@
 # # -*- coding: utf-8 -*-
 """Flask authentication with AWS Cognito IdP in USER_PASSWORD_AUTH authentication model"""
 
+from environs import Env
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
 
 class Config(object):
     """Base config, uses staging settings"""
     AWS_REGION = "eu-west-1"
-    COGNITO_USER_POOL_ID = "eu-west-1_GGFFHHJJ"
-    COGNITO_CLIENT_ID = "utyiutyityityuituyity"
-    COGNITO_CLIENT_SECRET = "fasdfasdfasdfasdfasdfasdfasdfasdfasdfdfasfasdfas"
+    COGNITO_USER_POOL_ID = env("COGNITO_USER_POOL_ID") or "eu-west-1_XXYYZZ"
+    COGNITO_CLIENT_ID = env("COGNITO_CLIENT_ID") or "ASDFG"
+    COGNITO_CLIENT_SECRET = env("COGNITO_CLIENT_SECRET") or "ZXCVBNM"
     COGNITO_AUTH_FLOW = 'USER_PASSWORD_AUTH'
 
 
